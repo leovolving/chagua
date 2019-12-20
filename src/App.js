5dccd38316738b9ab24753ebaede5f1f1c03180c
Leo Yockey <leoyockey@gmail.com>
2019-12-19 19:21:37 -0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, {Fragment} from 'react';
+import Chagua from './components/Chagua.jsx'
+
+function App() {
+  return (
+    <Fragment>
+      <header>
+        <h1>Chagua</h1>
+      </header>
+      <section>
+        <Chagua />
+      </section>
+    </Fragment>
+  );
+}
+
+export default App;
---
+++ src/components/Chagua.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class Chagua extends React.Component {
+    render () {
+        return (
+            <React.Fragment>
+                <div>Foo</div>
+            </React.Fragment>
+        );
+    }
+}
