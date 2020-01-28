d7ebe8c94fd587c5ec94e2923b868f40fae70dfe
Leo Yockey <leoyockey@gmail.com>
2020-01-27 19:16:49 -0800

    setup theme

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import React, {Fragment} from 'react';
-import Chagua from './components/Chagua.jsx'
+import React from 'react';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
+import Chagua from './components/Chagua.jsx'
+import green from '@material-ui/core/colors/green';
+import indigo from '@material-ui/core/colors/indigo';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: green[500],
+      main: green[700],
+      dark: green[900]
+    },
+    secondary: {
+      light: indigo[500],
+      main: indigo[700],
+      dark: indigo[900]
+    }
+  }
+});
 
 function App() {
   return (
-    <Fragment>
+    <ThemeProvider theme={theme}>
       <header>
         <Typography variant='h1'>Chagua</Typography>
         <p>Enter the options and the computer will randomly select a decision.</p>
@@ -12,7 +30,7 @@ function App() {
       <section>
         <Chagua />
       </section>
-    </Fragment>
+    </ThemeProvider>
   );
 }
 
--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,9 +1,36 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
 
-const ItemList = ({items}) => (
-    <ul>
-        {items.map((i, k) => <li key={k}><p>{i.value}</p></li>)}
-    </ul>
-);
+const useStyles = makeStyles(theme => ({
+    root: {
+      width: '100%',
+      maxWidth: 360,
+      backgroundColor: theme.palette.background.paper,
+    },
+    listItem: {
+        '&:nth-of-type(odd)': {
+            backgroundColor: '#eee'
+        },
+        '&:nth-of-type(even)': {
+            backgroundColor: '#edf3d8'
+        }
+    }
+  }));
+
+const ItemList = ({items}) => {
+    const classes = useStyles();
+    return (
+        <List className={classes.root}>
+            {items.map((i, k) => <ListItem className={classes.listItem} key={k}>
+                        <ListItemText>
+                            {i.value}
+                        </ListItemText>
+                </ListItem>)}
+        </List>
+    );
+}
 
 export default ItemList;
